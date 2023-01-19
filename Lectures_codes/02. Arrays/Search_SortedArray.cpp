6007a8cb7d224d3b8c872d5d25531d7160d5ef79
Anas Hany <96449333+AnasHany219@users.noreply.github.com>
2023-01-19 13:39:19 +0200

    Update Search_SortedArray.cpp

--- Lectures_codes/02. Arrays/Search_SortedArray.cpp
+++ Lectures_codes/02. Arrays/Search_SortedArray.cpp
@@ -1,1 +1,33 @@
+#include<iostream>
+using namespace std;
 
+// Binary Search deals with sorted array
+int BinarySearch(int arr[], int low, int high, int key){
+
+    if(high < low)
+        return -1;
+    int mid = (high + low) / 2;
+    if(arr[mid] == key)
+        return mid;
+    if(arr[mid] < key)
+        return BinarySearch(arr, mid + 1, high, key);
+    else
+        return BinarySearch(arr, low, mid - 1, key);
+}
+int main(){
+
+    int arr[] = {5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int key = 10;
+
+    int index = BinarySearch(arr, 0, n-1, key);
+    cout << "Index: " << index;
+    return 0;
+}
+
+//end main()
+/*
+-----------Output-----------
+Index: 5
+*/
